6565b3047991a108c4c761b8d95b1378ef95013b
Hadar Hai <64587231+hadar-hai@users.noreply.github.com>
2024-03-22 19:09:58 +0200

    Add files via upload

--- useful_functions.py
+++ useful_functions.py
@@ -109,3 +109,46 @@ def distance_to_obstacle(player_x, player_y, obstacle_x, obstacle_y, width, heig
     # find the closest point on the rectangular obstacle to the player
     closest_x, closest_y = closest_point_on_rectangle(top_left, top_right, bottom_left, bottom_right, (player_x, player_y))
     return euclidean_distance(player_x, player_y, closest_x, closest_y)
+
+def points_in_radius_on_rectangular(top_left, top_right, bottom_left, bottom_right, center, radius):
+    points = []
+    # find the center of the rectangle
+    rectangle_center = ((top_left[0] + top_right[0]) / 2, (top_left[1] + bottom_left[1]) / 2)
+    # find the distance between the center of the rectangle and the center of the circle
+    distance = euclidean_distance(rectangle_center[0], rectangle_center[1], center[0], center[1])
+    # if the distance is greater than the radius of the circle + the diagonal of the rectangle
+    # then the circle is outside the rectangle
+    if distance > radius + np.sqrt((top_right[0] - top_left[0]) ** 2 + (top_right[1] - top_left[1]) ** 2):
+        return points
+    # if the distance is less than the radius of the circle - the diagonal of the rectangle
+    # then the circle is inside the rectangle, return all points of the rectangle perimeter
+    if distance < radius - np.sqrt((top_right[0] - top_left[0]) ** 2 + (top_right[1] - top_left[1]) ** 2):
+        for x in range(int(top_left[0]), int(top_right[0])):
+            points.append((x, int(top_left[1])))
+            points.append((x, int(bottom_left[1])))
+        for y in range(int(top_left[1]), int(bottom_left[1])):
+            points.append((int(top_left[0]), y))
+            points.append((int(top_right[0]), y))
+        return points
+    # if the circle is intersecting the rectangle
+    # find the points on the rectangle perimeter only that are inside the circle 
+    # and add them to the list of points
+    for x in range(int(top_left[0]), int(top_right[0])):
+        if euclidean_distance(center[0], center[1], x, top_left[1]) <= radius:
+            points.append((x, top_left[1]))
+
+    # Iterate over the right side of the rectangle
+    for y in range(int(top_right[1]), int(bottom_left[1])):
+        if euclidean_distance(center[0], center[1], top_right[0], y) <= radius:
+            points.append((top_right[0], y))
+
+    # Iterate over the bottom side of the rectangle
+    for x in range(int(bottom_left[0]), int(top_left[0]), -1):
+        if euclidean_distance(center[0], center[1], x, bottom_left[1]) <= radius:
+            points.append((x, bottom_left[1]))
+
+    # Iterate over the left side of the rectangle
+    for y in range(int(bottom_left[1]), int(top_left[1]), -1):
+        if euclidean_distance(center[0], center[1], bottom_left[0], y) <= radius:
+            points.append((bottom_left[0], y))
+    return points 
